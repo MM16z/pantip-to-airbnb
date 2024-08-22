94a37e6df186bcf89cc04c6bacee500782afea87
MM16z <76926089+MM16z@users.noreply.github.com>
2024-08-22 09:43:47 +0700

    feat(cardUi): add em text handler

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,26 +10,24 @@ type pagePropType = {
 };
 
 export default function CardComponent({ contentImageUrl, contentTitle, author }: pagePropType) {
-  // function parseStringToReact(input: any) {
-  //   const parts = input?.split(/(\{\{em\}\}|\{\{eem\}\})/);
-  //   let isEmphasized = false;
+  function parseStringToReact(input: any) {
+    const parts = input?.split(/(\{\{em\}\}|\{\{eem\}\})/);
+    let isEmphasized = false;
 
-  //   return parts?.map((part: any, index: any) => {
-  //     if (part === '{{em}}') {
-  //       isEmphasized = true;
-  //       return null; // We handle opening tag implicitly
-  //     } else if (part === '{{eem}}') {
-  //       isEmphasized = false;
-  //       return null; // We handle closing tag implicitly
-  //     } else if (isEmphasized) {
-  //       // eslint-disable-next-line react/no-array-index-key
-  //       return <em key={index}>{part}</em>;
-  //     } else {
-  //       // eslint-disable-next-line react/no-array-index-key
-  //       return <span key={index}>{part}</span>; // Wrap non-emphasized parts in span to ensure they are React elements
-  //     }
-  //   });
-  // }
+    return parts?.map((part: any, index: any) => {
+      if (part === '{{em}}') {
+        isEmphasized = true;
+        return null;
+      } else if (part === '{{eem}}') {
+        isEmphasized = false;
+        return null;
+      } else if (isEmphasized) {
+        return <em key={index}>{part}</em>;
+      } else {
+        return <span key={index}>{part}</span>;
+      }
+    });
+  }
   return (
     <Card sx={{ maxWidth: 345, border: 'inherit', boxShadow: 'inherit', alignSelf: 'flex-start' }}>
       <CardActionArea className="flex flex-col items-center">
@@ -43,8 +41,7 @@ export default function CardComponent({ contentImageUrl, contentTitle, author }:
         />
         <CardContent className="p-3 sm:p-3">
           <Typography gutterBottom component="div" className="mb-2 text-sm font-bold">
-            {/* {parseStringToReact(contentTitle)} */}
-            {contentTitle}
+            {parseStringToReact(contentTitle)}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {author}
